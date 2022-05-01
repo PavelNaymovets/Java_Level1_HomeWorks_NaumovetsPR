2e48f2de2bfa7f5fdb20a2923cf5a0dc1c5b998c
Pavel Naumovets <pavel.naumovets@mail.ru>
2022-05-01 17:57:18 +0300

    Commit for Home Work 6

---
+++ src/main/java/ru/gb/naumovets/homeWork6/Cat.java
@@ -0,0 +1,32 @@
+package ru.gb.naumovets.homeWork6;
+
+public class Cat extends Animal{
+
+    private int staminaForRun = 200;
+    private static int count;
+
+
+    public Cat(String name){
+        super(name);
+        count++;
+    }
+
+    @Override
+    public void run(int distance){
+        if(distance > staminaForRun) {
+            System.out.println("Кот(шка) устал(а). Он(она) не может пробежать больше чем " + staminaForRun + " м. Пожалуйста, укажите дистанцию поменьше");
+        } else {
+            super.run(distance);
+        }
+    }
+
+    @Override
+    public void swim(int distance) {
+        System.out.println("Коты и кошки не умеют плавать");
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+}
