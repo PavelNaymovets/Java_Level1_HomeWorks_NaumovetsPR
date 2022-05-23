c45a0d23c399e727208bf4aef93d347e49d82556
Pavel Naumovets <pavel.naumovets@mail.ru>
2022-05-23 19:23:06 +0300

    First and last commit for home work 8

---
+++ src/main/java/ru/gb/naumovets/homeWork8/Main.java
@@ -0,0 +1,9 @@
+package ru.gb.naumovets.homeWork8;
+/*
+Используйте класс Main для запуска программы Calculator
+ */
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+    }
+}
